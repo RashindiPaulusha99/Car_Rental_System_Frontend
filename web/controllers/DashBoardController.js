63699e7569edd121d30210f906dad765f05ff318
RashindiPaulusha99 <rashindipaulusha837@gmail.com>
2022-07-26 03:00:28 +0530

    add colours to side navigation

--- web/controllers/DashBoardController.js
+++ web/controllers/DashBoardController.js
@@ -689,6 +689,7 @@ $("#btnLogToSystem").click(function () {
     $('#RegisterPage').css('transform','scale(0)');
 
     $("#AdminPanel").css('display','block');
+    $("#mainForm").css('display','block');
     $("#moreSpace").css('display','none');
     $("#NavBar").css('display','none');
     $("#CustomerDashBoard").css('display','none');
@@ -716,7 +717,6 @@ $("#btnLogToSystem").click(function () {
     $("#PaymentPage").css('display','none');
     $("#IncomePage").css('display','none');
     $("#Reservation").css('display','none');
-    $("#mainForm").css('display','none');
 });
 
 $("#btnLogOut").click(function () {
@@ -788,3 +788,286 @@ $("#openViewCars").click(function () {
     $("#Reservation").css('display','none');
     $("#mainForm").css('display','none');
 });
+
+$("#openDashboard").click(function () {
+    $('#login_page_container').css('transform','scale(0)');
+    $('#ForgotPasswordPage').css('transform','scale(0)');
+    $('#RegisterPage').css('transform','scale(0)');
+
+    $("#AdminPanel").css('display','block');
+    $("#mainForm").css('display','block');
+    $("#NavBar").css('display','none');
+    $("#WelcomePage").css('display','none');
+    $("#ViewCars").css('display','none');
+    $("#Services").css('display','none');
+    $("#OurCompany").css('display','none');
+    $("#OurHistory").css('display','none');
+    $("#OurTeam").css('display','none');
+    $("#Testimony").css('display','none');
+    $("#Blog").css('display','none');
+    $("#ContactInfoPage").css('display','none');
+    $("#CopyRightPage").css('display','none');
+    $("#CustomerDashBoard").css('display','none');
+    $("#moreSpace").css('display','none');
+    $("#CarManagePage").css('display','none');
+    $("#DriverManagePage").css('display','none');
+    $("#DriverDetailsPage").css('display','none');
+    $("#SchedulePage").css('display','none');
+    $("#ViewCustomerInformationPage").css('display','none');
+    $("#ViewReserveDetailsPage").css('display','none');
+    $("#ReserveDetailsPage").css('display','none');
+    $("#ChangeDriver").css('display','none');
+    $("#PaymentAddPage").css('display','none');
+    $("#PaymentPage").css('display','none');
+    $("#IncomePage").css('display','none');
+    $("#Reservation").css('display','none');
+    $("#CarDetailsPage").css('display','none');
+});
+
+$("#openViewDrivers").click(function () {
+    $('#login_page_container').css('transform','scale(0)');
+    $('#ForgotPasswordPage').css('transform','scale(0)');
+    $('#RegisterPage').css('transform','scale(0)');
+
+    $("#AdminPanel").css('display','block');
+    $("#DriverDetailsPage").css('display','block');
+    $("#NavBar").css('display','none');
+    $("#WelcomePage").css('display','none');
+    $("#ViewCars").css('display','none');
+    $("#Services").css('display','none');
+    $("#OurCompany").css('display','none');
+    $("#OurHistory").css('display','none');
+    $("#OurTeam").css('display','none');
+    $("#Testimony").css('display','none');
+    $("#Blog").css('display','none');
+    $("#ContactInfoPage").css('display','none');
+    $("#CopyRightPage").css('display','none');
+    $("#CustomerDashBoard").css('display','none');
+    $("#moreSpace").css('display','none');
+    $("#CarManagePage").css('display','none');
+    $("#DriverManagePage").css('display','none');
+    $("#SchedulePage").css('display','none');
+    $("#ViewCustomerInformationPage").css('display','none');
+    $("#ViewReserveDetailsPage").css('display','none');
+    $("#ReserveDetailsPage").css('display','none');
+    $("#ChangeDriver").css('display','none');
+    $("#PaymentAddPage").css('display','none');
+    $("#PaymentPage").css('display','none');
+    $("#IncomePage").css('display','none');
+    $("#Reservation").css('display','none');
+    $("#mainForm").css('display','none');
+    $("#CarDetailsPage").css('display','none');
+});
+
+$("#openViewCustomers").click(function () {
+    $('#login_page_container').css('transform','scale(0)');
+    $('#ForgotPasswordPage').css('transform','scale(0)');
+    $('#RegisterPage').css('transform','scale(0)');
+
+    $("#AdminPanel").css('display','block');
+    $("#ViewCustomerInformationPage").css('display','block');
+    $("#NavBar").css('display','none');
+    $("#WelcomePage").css('display','none');
+    $("#ViewCars").css('display','none');
+    $("#Services").css('display','none');
+    $("#OurCompany").css('display','none');
+    $("#OurHistory").css('display','none');
+    $("#OurTeam").css('display','none');
+    $("#Testimony").css('display','none');
+    $("#Blog").css('display','none');
+    $("#ContactInfoPage").css('display','none');
+    $("#CopyRightPage").css('display','none');
+    $("#CustomerDashBoard").css('display','none');
+    $("#moreSpace").css('display','none');
+    $("#CarManagePage").css('display','none');
+    $("#DriverManagePage").css('display','none');
+    $("#DriverDetailsPage").css('display','none');
+    $("#SchedulePage").css('display','none');
+    $("#ViewReserveDetailsPage").css('display','none');
+    $("#ReserveDetailsPage").css('display','none');
+    $("#ChangeDriver").css('display','none');
+    $("#PaymentAddPage").css('display','none');
+    $("#PaymentPage").css('display','none');
+    $("#IncomePage").css('display','none');
+    $("#Reservation").css('display','none');
+    $("#mainForm").css('display','none');
+    $("#CarDetailsPage").css('display','none');
+});
+
+$("#openSchedule").click(function () {
+    $('#login_page_container').css('transform','scale(0)');
+    $('#ForgotPasswordPage').css('transform','scale(0)');
+    $('#RegisterPage').css('transform','scale(0)');
+
+    $("#AdminPanel").css('display','block');
+    $("#SchedulePage").css('display','block');
+    $("#NavBar").css('display','none');
+    $("#WelcomePage").css('display','none');
+    $("#ViewCars").css('display','none');
+    $("#Services").css('display','none');
+    $("#OurCompany").css('display','none');
+    $("#OurHistory").css('display','none');
+    $("#OurTeam").css('display','none');
+    $("#Testimony").css('display','none');
+    $("#Blog").css('display','none');
+    $("#ContactInfoPage").css('display','none');
+    $("#CopyRightPage").css('display','none');
+    $("#CustomerDashBoard").css('display','none');
+    $("#moreSpace").css('display','none');
+    $("#CarManagePage").css('display','none');
+    $("#DriverManagePage").css('display','none');
+    $("#DriverDetailsPage").css('display','none');
+    $("#ViewCustomerInformationPage").css('display','none');
+    $("#ViewReserveDetailsPage").css('display','none');
+    $("#ReserveDetailsPage").css('display','none');
+    $("#ChangeDriver").css('display','none');
+    $("#PaymentAddPage").css('display','none');
+    $("#PaymentPage").css('display','none');
+    $("#IncomePage").css('display','none');
+    $("#Reservation").css('display','none');
+    $("#mainForm").css('display','none');
+    $("#CarDetailsPage").css('display','none');
+});
+
+$("#openViewRentals").click(function () {
+    $('#login_page_container').css('transform','scale(0)');
+    $('#ForgotPasswordPage').css('transform','scale(0)');
+    $('#RegisterPage').css('transform','scale(0)');
+
+    $("#AdminPanel").css('display','block');
+    $("#ViewReserveDetailsPage").css('display','block');
+    $("#NavBar").css('display','none');
+    $("#WelcomePage").css('display','none');
+    $("#ViewCars").css('display','none');
+    $("#Services").css('display','none');
+    $("#OurCompany").css('display','none');
+    $("#OurHistory").css('display','none');
+    $("#OurTeam").css('display','none');
+    $("#Testimony").css('display','none');
+    $("#Blog").css('display','none');
+    $("#ContactInfoPage").css('display','none');
+    $("#CopyRightPage").css('display','none');
+    $("#CustomerDashBoard").css('display','none');
+    $("#moreSpace").css('display','none');
+    $("#CarManagePage").css('display','none');
+    $("#DriverManagePage").css('display','none');
+    $("#DriverDetailsPage").css('display','none');
+    $("#SchedulePage").css('display','none');
+    $("#ViewCustomerInformationPage").css('display','none');
+    $("#ReserveDetailsPage").css('display','none');
+    $("#ChangeDriver").css('display','none');
+    $("#PaymentAddPage").css('display','none');
+    $("#PaymentPage").css('display','none');
+    $("#IncomePage").css('display','none');
+    $("#Reservation").css('display','none');
+    $("#mainForm").css('display','none');
+    $("#CarDetailsPage").css('display','none');
+});
+
+$("#openViewPayments").click(function () {
+    $('#login_page_container').css('transform','scale(0)');
+    $('#ForgotPasswordPage').css('transform','scale(0)');
+    $('#RegisterPage').css('transform','scale(0)');
+
+    $("#AdminPanel").css('display','block');
+    $("#PaymentPage").css('display','block');
+    $("#NavBar").css('display','none');
+    $("#WelcomePage").css('display','none');
+    $("#ViewCars").css('display','none');
+    $("#Services").css('display','none');
+    $("#OurCompany").css('display','none');
+    $("#OurHistory").css('display','none');
+    $("#OurTeam").css('display','none');
+    $("#Testimony").css('display','none');
+    $("#Blog").css('display','none');
+    $("#ContactInfoPage").css('display','none');
+    $("#CopyRightPage").css('display','none');
+    $("#CustomerDashBoard").css('display','none');
+    $("#moreSpace").css('display','none');
+    $("#CarManagePage").css('display','none');
+    $("#DriverManagePage").css('display','none');
+    $("#DriverDetailsPage").css('display','none');
+    $("#SchedulePage").css('display','none');
+    $("#ViewCustomerInformationPage").css('display','none');
+    $("#ViewReserveDetailsPage").css('display','none');
+    $("#ReserveDetailsPage").css('display','none');
+    $("#ChangeDriver").css('display','none');
+    $("#PaymentAddPage").css('display','none');
+    $("#IncomePage").css('display','none');
+    $("#Reservation").css('display','none');
+    $("#mainForm").css('display','none');
+    $("#CarDetailsPage").css('display','none');
+});
+
+$("#openIncomeReports").click(function () {
+    $('#login_page_container').css('transform','scale(0)');
+    $('#ForgotPasswordPage').css('transform','scale(0)');
+    $('#RegisterPage').css('transform','scale(0)');
+
+    $("#AdminPanel").css('display','block');
+    $("#IncomePage").css('display','block');
+    $("#NavBar").css('display','none');
+    $("#WelcomePage").css('display','none');
+    $("#ViewCars").css('display','none');
+    $("#Services").css('display','none');
+    $("#OurCompany").css('display','none');
+    $("#OurHistory").css('display','none');
+    $("#OurTeam").css('display','none');
+    $("#Testimony").css('display','none');
+    $("#Blog").css('display','none');
+    $("#ContactInfoPage").css('display','none');
+    $("#CopyRightPage").css('display','none');
+    $("#CustomerDashBoard").css('display','none');
+    $("#moreSpace").css('display','none');
+    $("#CarManagePage").css('display','none');
+    $("#DriverManagePage").css('display','none');
+    $("#DriverDetailsPage").css('display','none');
+    $("#SchedulePage").css('display','none');
+    $("#ViewCustomerInformationPage").css('display','none');
+    $("#ViewReserveDetailsPage").css('display','none');
+    $("#ReserveDetailsPage").css('display','none');
+    $("#ChangeDriver").css('display','none');
+    $("#PaymentAddPage").css('display','none');
+    $("#Reservation").css('display','none');
+    $("#mainForm").css('display','none');
+    $("#CarDetailsPage").css('display','none');
+    $("#PaymentPage").css('display','none');
+});
+
+$("#btnLogOutFromAdminPanel").click(function () {
+    $('#login_page_container').css('transform','scale(0)');
+    $('#ForgotPasswordPage').css('transform','scale(0)');
+    $('#RegisterPage').css('transform','scale(0)');
+
+    $("#NavBar").css('display','block');
+    $("#WelcomePage").css('display','block');
+    $("#ViewCars").css('display','block');
+    $("#Services").css('display','block');
+    $("#OurCompany").css('display','block');
+    $("#OurHistory").css('display','block');
+    $("#OurTeam").css('display','block');
+    $("#Testimony").css('display','block');
+    $("#Blog").css('display','block');
+    $("#ContactInfoPage").css('display','block');
+    $("#CopyRightPage").css('display','block');
+    $("#CustomerDashBoard").css('display','none');
+    $("#moreSpace").css('display','none');
+    $("#CarManagePage").css('display','none');
+    $("#CarDetailsPage").css('display','none');
+    $("#DriverManagePage").css('display','none');
+    $("#DriverDetailsPage").css('display','none');
+    $("#SchedulePage").css('display','none');
+    $("#ViewCustomerInformationPage").css('display','none');
+    $("#ViewReserveDetailsPage").css('display','none');
+    $("#ReserveDetailsPage").css('display','none');
+    $("#ChangeDriver").css('display','none');
+    $("#PaymentAddPage").css('display','none');
+    $("#PaymentPage").css('display','none');
+    $("#AdminPanel").css('display','none');
+    $("#IncomePage").css('display','none');
+    $("#Reservation").css('display','none');
+    $("#mainForm").css('display','none');
+});
+
+
+

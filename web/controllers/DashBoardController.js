50be12110ef3eb27880fe38330dc14c1b640cd8f
RashindiPaulusha99 <rashindipaulusha837@gmail.com>
2022-07-25 12:58:08 +0530

    add buttons to  footer

--- web/controllers/DashBoardController.js
+++ web/controllers/DashBoardController.js
@@ -196,6 +196,40 @@ $("#btnRegisterInLoginPage").click(function () {
     $("#RentCars").css('display','none');
 });
 
+$("#btnSighUp").click(function () {
+    $('#login_page_container').css('transform','scale(0)');
+    $('#ForgotPasswordPage').css('transform','scale(0)');
+    $('#RegisterPage').css('transform','scale(1)');
+
+    $("#NavBar").css('display','block');
+    $("#WelcomePage").css('display','block');
+    $("#ViewCars").css('display','none');
+    $("#Services").css('display','none');
+    $("#OurCompany").css('display','none');
+    $("#OurHistory").css('display','none');
+    $("#OurTeam").css('display','none');
+    $("#Testimony").css('display','none');
+    $("#Blog").css('display','none');
+    $("#ContactInfoPage").css('display','none');
+    $("#CopyRightPage").css('display','none');
+    $("#CarManagePage").css('display','none');
+    $("#CarDetailsPage").css('display','none');
+    $("#DriverManagePage").css('display','none');
+    $("#DriverDetailsPage").css('display','none');
+    $("#SchedulePage").css('display','none');
+    $("#ViewCustomerInformationPage").css('display','none');
+    $("#ViewReserveDetailsPage").css('display','none');
+    $("#ReserveDetailsPage").css('display','none');
+    $("#ChangeDriver").css('display','none');
+    $("#PaymentAddPage").css('display','none');
+    $("#PaymentPage").css('display','none');
+    $("#AdminPanel").css('display','none');
+    $("#IncomePage").css('display','none');
+    $("#Reservation").css('display','none');
+    $("#CustomerDashBoard").css('display','none');
+    $("#RentCars").css('display','none');
+});
+
 $("#btnOpenRegisterForm").click(function () {
     $('#login_page_container').css('transform','scale(0)');
     $('#ForgotPasswordPage').css('transform','scale(0)');
